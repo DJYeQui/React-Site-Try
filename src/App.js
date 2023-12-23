83a224ab1409f3113dbb1b58c02021eebbb05cac
DJYeQui <fatihanamasli@gmail.com>
2023-12-24 01:36:21 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Greetings from './Components/Greetings';
 
 function App() {
   return (
     <div className="App">
+        <Greetings />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components/Greetings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Greetings() {
+  return <div>
+      <h1>Hello World!</h1>
+  </div>
+}
+
+export default Greetings;
